23c756940287234c14bb3ed545b77930667aee32
iFoxyi <hestonesto123@gmail.com>
2026-08-19 12:04:36 +0000

    i just did my drill2

---
+++ Part1/Chapter2/Drill/drill2.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <memory>
+
+int main() {
+  std::cout << "How are u . . . ?" << std::endl;
+  std::string answer = "";
+  std::cin >> answer;
+  std::cout << " i miss u :> " << std::endl;
+  return 0;
+}

9b911cdf273c61c3fdacb9a7b8bf9f7c18386b19
iFoxyi <hestonesto123@gmail.com>
2026-08-20 13:05:14 +0000

    we made the expression example

---
+++ Part1/Chapter3/expressions.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  int length = 20; // literal integer
+  int width = 40;
+  int area =
+      length * width; // variable named area looking for object called length
+                      // and width to use the "*" expprestion to it
+
+  std::cout << area << std::endl;
+  return 0;
+}

32a1d6aff794c4cd0b7ae07faa345031bccea466
iFoxyi <hestonesto123@gmail.com>
2026-08-20 11:22:05 +0000

    we added the rest of the tasks in obsidian untill we get the knowldge enough then we will work on them

---
+++ Part1/Chapter2/Excercises/exc6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  std::cout << "Can u please Enter three integers : " << std::endl;
+  int value1 = 0;
+  int value2 = 0;
+  int value3 = 0;
+  std::cin >> value1 >> value2 >> value3;
+
+  if (value1 < value2) {
+  }
+}

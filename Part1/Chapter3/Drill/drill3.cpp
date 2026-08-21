cd84fe5d5246a03b6d038255251725aaeed18182
iFoxyi <hestonesto123@gmail.com>
2026-08-21 17:12:51 +0000

    we just finished with the drill number 3 in chapter 3

---
+++ Part1/Chapter3/Drill/drill3.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+  int value1 = 0;
+  int value2 = 0;
+
+  std::cout << "Enter two integers (or '|' to quit):\n";
+  while (std::cin >> value1 >> value2) {
+    std::cout << "The values are: " << value1 << " and " << value2 << "\n";
+    if (value1 > value2) {
+      std::cout << value1 << " is larger than " << value2 << std::endl;
+    } else if (value2 == value1) {
+      std::cout << "Values are equall . . . " << std::endl;
+    }
+
+    else {
+      std::cout << value2 << " is larger than " << value1 << std::endl;
+    }
+    std::cout << "Enter two integers (or '|' to quit):\n";
+  }
+  return 0;
+}

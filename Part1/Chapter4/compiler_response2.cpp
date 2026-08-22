85c2d65e8eb618a4bbf4d0271889da2dfffb7dcf
iFoxyi <hestonesto123@gmail.com>
2026-08-22 06:54:01 +0000

    we tried new ways of possible errors

---
+++ Part1/Chapter4/compiler_response2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int area(int a, int b) { return a * b; }
+
+int main() {
+  std::cout << arena(1, 5);
+  std::cout << area(1);
+  std::cout << area("sadasd", 5);
+
+  return 0;
+}

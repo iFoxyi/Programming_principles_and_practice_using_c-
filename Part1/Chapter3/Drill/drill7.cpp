368073c7b48609f6ab125abb8f1cbe8e590fe3ad
iFoxyi <hestonesto123@gmail.com>
2026-08-22 04:30:23 +0000

    we just done wih drill number 7 in chapter 3

---
+++ Part1/Chapter3/Drill/drill7.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  std::cout << "can u please enter the value followed by a unit (in,m,ft): "
+            << std::endl;
+  double value = 0.00;
+  std::string unit = " ";
+
+  while (std::cin >> value >> unit) {
+    if (unit == "in") {
+      std::cout << value << "in" << " == " << value * 2.54 << "cm" << std::endl;
+    } else if (unit == "m") {
+      std::cout << value << "m" << " == " << value * 100 << "cm" << std::endl;
+    } else if (unit == "ft") {
+      std::cout << value << "ft" << "== " << value * 12 << "in" << std::endl;
+    }
+  }
+
+  return 0;
+}

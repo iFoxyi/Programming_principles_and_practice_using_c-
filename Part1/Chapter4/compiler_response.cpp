7a150616a9e4d36141d19766ac2a08c826241969
iFoxyi <hestonesto123@gmail.com>
2026-08-22 06:45:57 +0000

    we see how the compiler will respond

---
+++ Part1/Chapter4/compiler_response.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int area(int length, int width) {
+  int result = length * width;
+  return result;
+}
+int main() {
+  std::cout << area('5', 4);
+  std::cout << area(2, 3)
+  std::cout << area(4,5;
+
+  return 0;
+}
